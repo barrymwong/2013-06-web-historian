98d0bad38f17e62d365dc759d8aafb38dbfe2c70
Hack Reactor Students <github@hackreactor.com>
2013-07-10 11:16:11 -0700

    request-handler responds 200 on get request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,10 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
 
 exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  // response 200 to a GET
+  // check URL
+  // end a response
+  if(req.method == "GET"){
+    res.writeHead(200);
+  }
 };
