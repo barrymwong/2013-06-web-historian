1659c348c7e6dcc252d3c9629812a89108486c7a
Hack Reactor Students <github@hackreactor.com>
2013-07-10 15:39:11 -0700

    request-handler passes all tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,19 +3,30 @@ var url = require('url');
 var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  // handle POSTs
-  // respond with end
-  // respond writeHead 320
-  // write the input data to fileContents
 
   var path = url.parse(req.url).path.split('/');
+
   if(req.method === "GET"){
-    res.writeHead(200);
     var result = '<input>';
+
+    var fileContents = fs.readFileSync(exports.datadir, 'utf8');
+    var re = new RegExp(path[1]);
+    var test = re.test(fileContents);
+
     if(path[1]){
-      result = path[1];
+      if(test){
+        res.writeHead(200);
+        result = path[1];
+        res.end(result);
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    } else {
+      res.writeHead(200);
+      res.end(result);
     }
-    res.end(result);
+
   } else if(req.method === 'POST') {
     var data;
     req.on('data', function(chunk) {
@@ -28,6 +39,4 @@ exports.handleRequest = function (req, res) {
       res.end();
     });
   }
-
-
 };
